92502301fef01a205477dae6ccc9ff1b42874907
Ashish Samarth <mrashishsamarth@gmail.com>
2023-01-15 21:37:08 -0600

    start_db

---
+++ OracleDB/oracle-bash-scripts/start_db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export TMP=/tmp
+export TMPDIR=$TMP
+export ORACLE_HOSTNAME=ol7
+export ORACLE_UNQNAME=cdb19c
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=$ORACLE_BASE/product/db/19.0.0/dbhome_1
+export ORA_INVENTORY=/u01/app/oraInventory
+export ORACLE_SID=cdb19c
+export PDB_NAME=samarth_db_sandbox
+
+ORAENV_ASK=NO
+. oraenv
+ORAENV_ASK=YES
+
+# Start Listener
+lsnrctl start
+
+# Start Database
+sqlplus / as sysdba << EOF
+
+STARTUP;
+EXIT;
+EOF
