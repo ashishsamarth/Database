c7a1ff0902db976d27a42f3e9b6a2023e0fd8a18
ashish samarth <mrashishsamarth@gmail.com>
2023-01-31 07:29:34 -0600

    stop_db

--- OracleDB/oracle-bash-scripts/stop_db.sh
+++ OracleDB/oracle-bash-scripts/stop_db.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-export TMP=/tmp
-export TMPDIR=$TMP
+. /home/oracle/scripts/set_env.sh
 
-export ORACLE_HOSTNAME=$HOSTNAME
-export ORACLE_UNQNAME=cdb19c
-export ORACLE_BASE=/u01/app/oracle
-export ORACLE_HOME=$ORACLE_BASE/product/db/19.0.0/dbhome_1
-export ORA_INVENTORY=/u01/app/oraInventory
-export ORACLE_SID=cdb19c
-export PDB_NAME=samarth_db_sandbox
-
-ORAENV_ASK=NO
+export ORAENV_ASK=NO
 . oraenv
-ORAENV_ASK=YES
+export ORAENV_ASK=YES
 
-# Stop Database
-sqlplus / as sysdba << EOF
-
-SHUTDOWN IMMEDIATE;
-EXIT;
-EOF
-
-# Stop Listener
-lsnrctl stop
+dbshut $ORACLE_HOME
